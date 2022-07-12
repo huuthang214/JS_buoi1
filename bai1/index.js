d149069227847a4521cf1b88cf52ba0f06b21f99
huuthang214 <huuthangqn99@gmail.com>
2022-07-12 10:26:34 +0700

    complete

---
+++ bai1/index.js
@@ -0,0 +1,16 @@
+/*
+Input:  -Luong 1 ngay
+        -So ngay lam
+
+Xử Lý: luong = luong 1 ngay * so ngay lam
+
+Output:
+Luong nhan vien
+
+*/
+const salaryADay = 100000;
+var numberDay = 20;
+
+totalSalary = salaryADay * numberDay;
+
+console.log("total salary: " + totalSalary); 
---
+++ bai2/index.js
@@ -0,0 +1,11 @@
+/*
+INPUT : nhập 5 số thực a, b, c, d, e
+
+giải thuật: cộng tổng 5 số thực và chia cho 5 
+
+OUTPUT : GTTB của 5 số thực
+
+*/
+var a = 5, b = 4.5, c = 5.7, d = 6.5, e = 9.8;
+avg = (a + b + c + d + e)/5;
+console.log(avg);
